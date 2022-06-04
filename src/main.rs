8fd55db28e8353c2fc48e1467c6be75951c8f7d1
hikikones <hikikones@gmail.com>
2022-06-04 20:21:50 +0200

    pages module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use dioxus::prelude::*;
 
+mod pages;
+
 fn main() {
     dioxus::desktop::launch(app);
 }
@@ -14,48 +16,11 @@ fn app(cx: Scope) -> Element {
                 Link { to: "/add_card", li { "Add card"  }}
                 Link { to: "/edit_card/1", li { "Edit card"  }}
             }
-            Route { to: "/", Home {} }
-            Route { to: "/cards", Cards {} }
-            Route { to: "/add_card", AddCard {} }
-            Route { to: "/edit_card/:id", EditCard {} }
+            Route { to: "/", pages::Home {} }
+            Route { to: "/cards", pages::Cards {} }
+            Route { to: "/add_card", pages::AddCard {} }
+            Route { to: "/edit_card/:id", pages::EditCard {} }
             Redirect { from: "", to: "/" }
-        }
-    })
-}
-
-#[allow(non_snake_case)]
-fn Home(cx: Scope) -> Element {
-    cx.render(rsx! {
-        h1 { "Home" }
-    })
-}
-
-#[allow(non_snake_case)]
-fn Cards(cx: Scope) -> Element {
-    cx.render(rsx! {
-        h1 { "Cards" }
-    })
-}
-
-#[allow(non_snake_case)]
-fn AddCard(cx: Scope) -> Element {
-    cx.render(rsx! {
-        h1 { "Add card" }
-    })
-}
-
-#[allow(non_snake_case)]
-fn EditCard(cx: Scope) -> Element {
-    let id = use_route(&cx)
-        .segment("id")
-        .unwrap()
-        .parse::<usize>()
-        .unwrap_or(0);
-
-    cx.render(rsx! {
-        div {
-            h1 { "Edit card" }
-            p { "Card to edit: {id}" }
         }
     })
 }
