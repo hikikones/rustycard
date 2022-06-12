75fe681c6005a172117cccd8acf1877435332993
hikikones <hikikones@gmail.com>
2022-06-12 22:58:56 +0200

    oninput callback

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,26 +1,32 @@
-use dioxus::prelude::*;
+use dioxus::{events::FormEvent, prelude::*};
 
 #[allow(non_snake_case)]
 pub fn Home(cx: Scope) -> Element {
+    let md = "Yoyo";
+
     cx.render(rsx! {
         h1 { "Home" }
-        TextArea { text: "Yoyo".into() }
+        TextArea {
+            value: md,
+            oninput: |evt| {
+                println!("{:?}", evt);
+            }
+        }
     })
 }
 
-#[derive(Props, PartialEq)]
-struct AppProps {
-    text: String,
+#[derive(Props)]
+struct TextAreaProps<'a> {
+    value: &'a str,
+    oninput: EventHandler<'a, FormEvent>,
 }
 
 #[allow(non_snake_case)]
-fn TextArea(cx: Scope<AppProps>) -> Element {
+fn TextArea<'a>(cx: Scope<'a, TextAreaProps<'a>>) -> Element {
     cx.render(rsx! {
         textarea {
-            oninput: |evt| {
-                println!("{:?}", evt);
-            },
-            "{cx.props.text}"
+            oninput: |evt| cx.props.oninput.call(evt),
+            "{cx.props.value}"
         }
     })
 }
