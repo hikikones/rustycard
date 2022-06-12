cc15564b31b6c3bc5cc6a8178187a23c7f0083fa
hikikones <hikikones@gmail.com>
2022-06-12 21:51:37 +0200

    oninput textarea

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -4,5 +4,23 @@ use dioxus::prelude::*;
 pub fn Home(cx: Scope) -> Element {
     cx.render(rsx! {
         h1 { "Home" }
+        TextArea { text: "Yoyo".into() }
+    })
+}
+
+#[derive(Props, PartialEq)]
+struct AppProps {
+    text: String,
+}
+
+#[allow(non_snake_case)]
+fn TextArea(cx: Scope<AppProps>) -> Element {
+    cx.render(rsx! {
+        textarea {
+            oninput: |evt| {
+                println!("{:?}", evt);
+            },
+            "{cx.props.text}"
+        }
     })
 }
