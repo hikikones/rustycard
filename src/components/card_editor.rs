dd1c470f78985c6dd57957cf8e4f1d4d60b4574b
hikikones <hikikones@gmail.com>
2022-06-20 04:09:39 +0200

    test native dialog

--- src/components/card_editor.rs
+++ src/components/card_editor.rs
@@ -1,4 +1,7 @@
+use std::ops::Deref;
+
 use dioxus::prelude::*;
+use native_dialog::FileDialog;
 
 use super::MarkdownView;
 
@@ -7,6 +10,28 @@ pub fn CardEditor<'a>(cx: Scope<'a, CardEditorProps<'a>>) -> Element {
     let content = use_state(&cx, || cx.props.initial_value.to_owned());
 
     cx.render(rsx! {
+        img { src: "assets/img.png" }
+        button {
+            onclick: |_| {
+                let path = FileDialog::new()
+                .set_location("~/Desktop")
+                .add_filter("PNG Image", &["png"])
+                .add_filter("JPEG Image", &["jpg", "jpeg"])
+                .show_open_single_file()
+                .unwrap();
+
+                if let Some(path) = path {
+                    println!("PATH: {:?}", path);
+                    // cx.props.onsave.call(content);
+                    // content.set(String::new());
+                    let mut md = content.current().deref().clone();
+                    md.push_str("\n");
+                    md.push_str(&format!("![]({})", path.display()));
+                    content.set(md);
+                }
+            },
+            "Image"
+        }
         textarea {
             rows: "10",
             cols: "80",
--- src/services/config.rs
+++ src/services/config.rs
@@ -5,21 +5,27 @@ pub struct Config(Rc<Cfg>);
 
 pub struct Cfg {
     pub db_file: PathBuf,
-    pub media_dir: PathBuf,
+    pub assets_dir: PathBuf,
 }
 
 impl Config {
     pub fn new() -> Self {
+        Self(Rc::new(Cfg {
+            db_file: "db.sqlite3".into(),
+            assets_dir: "assets/".into(),
+        }))
+
         // TODO: Parse or create config file.
+        // TODO: Set custom asset path.
 
-        let app_dirs = platform_dirs::AppDirs::new(Some("rustycard"), false).unwrap();
-        std::fs::create_dir_all(&app_dirs.data_dir).unwrap();
+        // let app_dirs = platform_dirs::AppDirs::new(Some("rustycard"), false).unwrap();
+        // std::fs::create_dir_all(&app_dirs.data_dir).unwrap();
 
-        let db_file = app_dirs.data_dir.join("database.sqlite3");
-        let media_dir = app_dirs.data_dir.join("media/");
-        std::fs::create_dir_all(&media_dir).unwrap();
+        // let db_file = app_dirs.data_dir.join("database.sqlite3");
+        // let media_dir = app_dirs.data_dir.join("media/");
+        // std::fs::create_dir_all(&media_dir).unwrap();
 
-        Self(Rc::new(Cfg { db_file, media_dir }))
+        // Self(Rc::new(Cfg { db_file, media_dir }))
     }
 }
 
