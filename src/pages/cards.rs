3b202f4d4c7e0ffa405d75f59f955f7e541ce55a
hikikones <hikikones@gmail.com>
2022-06-24 03:19:40 +0200

    cleanup

--- src/pages/cards.rs
+++ src/pages/cards.rs
@@ -4,23 +4,6 @@ use dioxus::prelude::*;
 
 use crate::{components::MarkdownView, services::database::*};
 
-#[derive(PartialEq, Props)]
-struct TagEl {
-    id: usize,
-    name: String,
-    selected: bool,
-}
-
-impl From<&Tag> for TagEl {
-    fn from(tag: &Tag) -> Self {
-        Self {
-            id: tag.id,
-            name: tag.name.clone(),
-            selected: false,
-        }
-    }
-}
-
 #[allow(non_snake_case)]
 pub fn Cards(cx: Scope) -> Element {
     let db = &*cx.use_hook(|_| cx.consume_context::<Database>().unwrap());
@@ -45,20 +28,17 @@ pub fn Cards(cx: Scope) -> Element {
         span {
             color: format_args!("{}", if **show_tagless {"blue"} else {"black"}),
             onclick: |_| {
-                // *show_tagless.make_mut() = !*show_tagless.current();
                 let mut show = false;
                 show_tagless.with_mut(|s| {
                     *s = !*s;
-                    // println!("SHOW: {}", *show_tagless);
                     show = *s;
                 });
-                // show_tagless.set(!*show_tagless.current());
-                println!("SHOW: {}", show);
+
                 if show {
                     cards.set(db.get_cards_without_tags());
-               } else {
+                } else {
                     cards.set(db.get_cards_by_tags(&selected_tags.current().iter().copied().collect::<Vec<_>>()));
-               }
+                }
             },
             "tagless",
         }
@@ -66,10 +46,8 @@ pub fn Cards(cx: Scope) -> Element {
         tags.iter().map(|t| rsx! {
             span {
                 key: "{t.id}",
-                // color: format_args!("{}", if t.selected {"blue"} else {"black"}),
                 color: format_args!("{}", if !**show_tagless && selected_tags.contains(&t.id) {"blue"} else {"black"}),
                 onclick: |_| {
-                    // println!("TAG ID: {}", t.id);
                     show_tagless.set(false);
                     selected_tags.with_mut(|tags| {
                         if tags.contains(&t.id) {
@@ -91,42 +69,5 @@ pub fn Cards(cx: Scope) -> Element {
                 text: "{c.content}",
             }
         })
-
-        // hr {}
-        // db.get_cards().iter().map(|c| rsx! {
-        //     MarkdownView {
-        //         key: "{c.id}",
-        //         text: "{c.content}",
-        //     }
-        // })
-
-        // h1 { "Tags" }
-        // hr {}
-        // ul {
-        //     db.get_tags().iter().map(|t| rsx! {
-        //         li {
-        //             key: "{t.id}",
-        //             "{t.name}",
-        //         }
-        //     })
-        // }
-
-        // h1 { "Cards with tag1 & tag2" }
-        // hr {}
-        // db.get_cards_by_tags(&[1,2]).iter().map(|c| rsx! {
-        //     MarkdownView {
-        //         key: "{c.id}",
-        //         text: "{c.content}",
-        //     }
-        // })
-
-        // h1 { "Tagless cards" }
-        // hr {}
-        // db.get_cards_without_tags().iter().map(|c| rsx! {
-        //     MarkdownView {
-        //         key: "{c.id}",
-        //         text: "{c.content}",
-        //     }
-        // })
     })
 }
