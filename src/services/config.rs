9a5a28d481df3d1e922ba89dd0a348059a62be8f
hikikones <hikikones@gmail.com>
2022-07-17 11:22:24 +0200

    app dir cfg debug release

--- src/services/config.rs
+++ src/services/config.rs
@@ -1,43 +1,43 @@
 use std::{ops::Deref, path::PathBuf, rc::Rc};
 
 #[derive(Clone)]
-pub struct Config(Rc<Cfg>);
+pub struct Config(Rc<ConfigData>);
 
-pub struct Cfg {
+pub struct ConfigData {
     pub db_file: PathBuf,
     pub assets_dir: PathBuf,
 }
 
 impl Config {
     pub fn new() -> Self {
-        let db_file = "db.sqlite3";
-        let assets_dir = "assets/";
+        let app_dir = get_app_dir();
+        let db_file = app_dir.join("rustycard.db");
+        let assets_dir = app_dir.join("assets/");
 
-        std::fs::create_dir_all(assets_dir).unwrap();
+        std::fs::create_dir_all(&assets_dir).unwrap();
 
-        Self(Rc::new(Cfg {
-            db_file: db_file.into(),
-            assets_dir: assets_dir.into(),
+        Self(Rc::new(ConfigData {
+            db_file,
+            assets_dir,
         }))
-
-        // TODO: Parse or create config file.
-        // TODO: Set custom asset path.
-
-        // let app_dirs = platform_dirs::AppDirs::new(Some("rustycard"), false).unwrap();
-        // std::fs::create_dir_all(&app_dirs.data_dir).unwrap();
-
-        // let db_file = app_dirs.data_dir.join("database.sqlite3");
-        // let media_dir = app_dirs.data_dir.join("media/");
-        // std::fs::create_dir_all(&media_dir).unwrap();
-
-        // Self(Rc::new(Cfg { db_file, media_dir }))
     }
 }
 
 impl Deref for Config {
-    type Target = Cfg;
+    type Target = ConfigData;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
+}
+
+#[cfg(debug_assertions)]
+fn get_app_dir() -> PathBuf {
+    ".".into()
+}
+
+#[cfg(not(debug_assertions))]
+fn get_app_dir() -> PathBuf {
+    let app_dirs = platform_dirs::AppDirs::new(Some("rustycard"), false).unwrap();
+    app_dirs.data_dir
 }
